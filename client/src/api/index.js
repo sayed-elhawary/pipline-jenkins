59f2dd91975a12a1b87d5c0e1301314ecca2f9e9
Sayed Elhawary <selhawary2025@hotmail.com>
2025-04-22 01:34:26 +0200

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://172.31.19.215:5000',
+    baseURL: 'http://localhost:5000/api',
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
